ca958b37a5e48dc9c4961d2e34e4e9c45cd1212a
Ivan <vanechka.egorov.2003@mail.ru>
2019-07-24 19:18:56 +0300

    initial commit

---
+++ js/index.js
@@ -0,0 +1,22 @@
+function convert(rubles) {
+  return Math.round(rubles / 63);
+}
+
+function outputResult(rubles) {
+  var dollars = convert(rubles);
+
+  $(".dollars").html(dollars + "$");
+}
+
+function init() {
+  $(".rubles").keyup(function() {
+    outputResult(this.value);
+  });
+
+  $(".btn").click(function() {
+    var rubles = $(".rubles").val();
+    outputResult(rubles);
+  });
+}
+
+$(init);
